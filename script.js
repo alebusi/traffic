9aa01311fa0d92f735050ab3d9826c370d8ff2f7
alebusi <allebusi@gmail.com>
2020-04-14 14:00:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var colori = ["white","orange","#ff3300"];
+var colori = ["white","#FF3300","orange","#4285F4","#0F9D58","black"];
 var ind_c = 0;
-var ind = 0;
-var ind_a = 0;
 var distanza = ["10px","30px","60px","100px","160px","300px"];
+var ind = 0;
+var dimensioni = ["20px","30px","50px","80px"];
+ind_dim = 0;
+var animazioni = ["vuoto","ruota","allarga"];
+ind_anim = 0;
 
 function cambioColore(td) {
   td.style.backgroundColor = colori[ind_c];
@@ -20,12 +23,19 @@ function cambioDistanza() {
   document.getElementById("griglia").style.borderSpacing=distanza[ind];
 }
 
-function cambioAnimazione() {
-  if (document.getElementById("griglia").className=="vuoto") {
-    document.getElementById("griglia").className="ruota";  
-  }
-  else {
-    document.getElementById("griglia").className="vuoto";   
+function cambioDimensione() {
+  ind_dim+=1;
+  if (ind_dim > dimensioni.length-1) ind_dim=0;
+  var x = document.getElementsByTagName("TD");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.width = dimensioni[ind_dim];
+    x[i].style.height = dimensioni[ind_dim];
   }
 }
 
+function cambioAnimazione() {
+  ind_anim+=1;
+  if (ind_anim > animazioni.length-1) ind_anim=0;
+  document.getElementById("griglia").className=animazioni[ind_anim];
+}

e2ac96a9b6c26b3092011a5fc5bd415ade5e6fa2
alebusi <allebusi@gmail.com>
2021-07-23 10:49:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var senso = ["orario","antiorario"];
 var colori = ["white","#FF3300","orange","#4285F4","#0F9D58","black"];
 var ind_c = 0;
 var distanza = ["30px","60px","100px","160px","200px"];
@@ -6,6 +7,41 @@ var dimensioni = ["20px","30px","50px","80px"];
 ind_dim = 0;
 var animazioni = ["vuoto","ruota","ruota_a","allarga"];
 ind_anim = 0;
+
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+// Disegna elementi
+let box = document.createElement('div');
+//box.id = 'content';
+box.className = senso[randomIntFromInterval(0, senso.length-1)];
+
+// create a new heading and add it to the div
+let car = document.createElement('div');
+box.appendChild(car);
+
+// add div to the document
+document.body.appendChild(box);
+
+/*
+var arrayDiv = new Array();
+        i=0;
+        arrayDiv[i] = document.createElement('div');
+        arrayDiv[i].id = 'block' + i;
+        arrayDiv[i].className = senso[randomIntFromInterval(0, senso.length-1)];
+        arrayDiv[i].style.top = y*(i+1)+"px";
+        if (x < 50) {
+            x=randomIntFromInterval(51, 80);
+        }
+        else {
+            x=randomIntFromInterval(20, 50);
+        }            
+        arrayDiv[i].style.left = x+"vw";
+        document.getElementById("area").appendChild(arrayDiv[i]);
+    }
+*/
 
 function cambioColore(td) {
   td.style.backgroundColor = colori[ind_c];
